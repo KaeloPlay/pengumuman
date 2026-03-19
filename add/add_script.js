066c964d672dae1e3f350e25bc406857e7f66aed
KaeloPlay <kaelo.swm@gmail.com>
2026-03-19 14:09:31 +0700

    added 'add pengumuman' page

---
+++ add/add_script.js
@@ -0,0 +1,76 @@
+const addPR = document.querySelector('#add-pr');
+const dropMapel = document.querySelector('#pr-mapel-drop');
+const prItemsTemp = document.querySelector('#pr-items-template')
+const prContainer = document.querySelector('.pr-container');
+const sendInput = document.querySelector('#sendInput');
+
+addPR.addEventListener('click', () => {
+    addPR.classList.add('disabled');
+    dropMapel.classList.remove('disabled');
+});
+
+dropMapel.addEventListener('change', () => {
+    const clone = prItemsTemp.content.cloneNode(true);
+    const mapelEl = clone.querySelector('.pr-items-mapel')
+    mapelEl.textContent = dropMapel.value + ':';
+    prContainer.appendChild(clone);
+
+    addPR.classList.remove('disabled')
+    dropMapel.classList.add('disabled')
+
+    const option = dropMapel.querySelector(`option[value='${dropMapel.value}']`);
+    option.disabled = true;
+    dropMapel.selectedIndex = 0;
+});
+
+prContainer.addEventListener('keydown', (e) => {
+    if (e.target.classList.contains('pr-details-input')) {
+        if (e.key === 'Backspace' && e.target.value === '') {
+            e.preventDefault();
+
+            const li = e.target.closest('li');
+            const mapel = li.querySelector('.pr-items-mapel').textContent.replace(':', '');
+
+            const option = dropMapel.querySelector(`option[value='${mapel}']`);
+            if (option) option.disabled = false;
+            
+           li.remove();
+        };
+    };
+});
+
+sendInput.addEventListener('click', () => {
+    const allPR = Array.from(document.querySelectorAll('.pr-items'))
+    .map(item => {
+        const mapel = item.querySelector('.pr-items-mapel').textContent;
+        const value = item.querySelector('.pr-details-input').value;
+
+        return `${mapel} ${value}`;
+    });
+
+    let pr = allPR.join(', ');
+    if (pr === '') prString = 'Tidak ada PR untuk esok hari.';
+
+    let tambahan = document.querySelector('#tambahanInput').value;
+    if (tambahan === '') tambahan = 'Tidak ada informasi tambahan untuk esok hari.';
+    
+    postData(pr, tambahan);
+    alert('Makasii sekle kecayangan kelas🥰')
+    location.reload();
+});
+
+async function postData(pr, tambahan) {
+    try {
+        const res = await fetch('https://api-pengumuman-production.up.railway.app/api/pengumuman', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                pr: pr, note: tambahan
+            })
+        });
+    } catch (err) {
+        console.error('Failed to post:', err)
+    }
+};
