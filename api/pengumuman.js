58570c8f887de590c1903553d8012ea2da720298
KaeloPlay <kaelo.swm@gmail.com>
2026-04-29 19:00:28 +0700

    added new endpoint, /redirect/ (url redirector)

--- api/pengumuman.js
+++ api/pengumuman.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '../lib/supabase'
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
---
+++ redirect/[key].js
@@ -0,0 +1,18 @@
+import { createClient, supabase } from '../lib/supabase'
+
+export default async function handler(req, res) {
+    const { key } = req.query;
+
+    const { data, error } = await supabase
+    .from('links')
+    .select('target')
+    .eq('key', key)
+    .single();
+
+    if (error || !data) {
+        return res.status(404).send('Link is not found')
+    }
+
+    res.writeHead(302, { location: data.target });
+    res.end();
+}
