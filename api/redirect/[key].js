a4b1619825a8440631cded89eab50b17460e5f53
KaeloPlay <kaelo.swm@gmail.com>
2026-04-29 19:44:18 +0700

    FIX

--- api/redirect/[key].js
+++ api/redirect/[key].js
@@ -1,4 +1,9 @@
-import { createClient } from '../../lib/supabase.js';
+import { createClient } from '@supabase/supabase-js'
+
+export const createClient = createSupabaseClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+)
 
 export default async function handler(req, res) {
     const { key } = req.query;
